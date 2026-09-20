429b378cb39e1b7e3acbf78805875c053bb13bd7
Tarek Khaled <tarekkhaled5112@gmail.com>
2026-09-21 01:47:28 +0300

    Develop CreateProductEndpoint with minimal apis using carter library & mapster for mapping objects

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,6 +1,29 @@
-﻿namespace Catalog.API.Products.CreateProduct
+﻿using Carter;
+using Mapster;
+using MediatR;
+
+namespace Catalog.API.Products.CreateProduct
 {
-    public class CreateProductEndpoint
+    public record CreateProductRequest
+        (string Name, string Description, string ImageUrl, List<string> Categories, decimal Price);
+
+    public record CreateProductResponse(Guid Id);
+    public class CreateProductEndpoint() : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/products", async (CreateProductRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<CreateProductCommand>();
+                var result = await sender.Send(command);
+                var responce = result.Adapt<CreateProductResponse>();
+                return Results.Created($"/products/{result.Id}", result.Id);
+            })
+            .WithName("CreateProduct")
+            .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Create a new product")
+            .WithDescription("Creates a new product with the specified details.");
+        }
     }
 }
