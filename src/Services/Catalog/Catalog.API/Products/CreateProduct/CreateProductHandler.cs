613b71274d9026a2f255f0683de40d3452158a08
Tarek Khaled <tarekkhaled5112@gmail.com>
2026-09-21 01:04:15 +0300

    Develop CreateProductHandler

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,6 +1,19 @@
-﻿namespace Catalog.API.Products.CreateProduct
+﻿using BuldingBlocks.CQRS;
+using Catalog.API.Models;
+
+namespace Catalog.API.Products.CreateProduct
 {
-    public class CreateProductHandler
+    public record CreateProductCommand
+    (string Name, string Description, string ImageUrl, decimal Price, List<string> Categories)
+    : ICommand<CreateProductResult>;
+
+    public record CreateProductResult(Guid Id);
+
+    internal class CreateProductHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
+        public async Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            return new CreateProductResult(Guid.NewGuid());
+        }
     }
 }
