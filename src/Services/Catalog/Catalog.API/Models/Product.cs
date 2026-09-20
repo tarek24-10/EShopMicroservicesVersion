e70c413c04a4980bbd3afa6d8614416664da64e3
Tarek Khaled <tarekkhaled5112@gmail.com>
2026-09-20 23:30:13 +0300

    Create domain model (product )

---
+++ src/Services/Catalog/Catalog.API/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace Catalog.API.Models
+{
+    public class Product
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; private set; } = default!;
+        public string Description { get; private set; } = default!;
+        public string ImageUrl { get; private set; } = default!; 
+        public decimal Price { get; private set; }
+        public List<string> Categories { get; private set; } = new();
+    }
+}
