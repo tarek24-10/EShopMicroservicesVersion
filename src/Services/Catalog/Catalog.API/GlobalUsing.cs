bc061783cd4760f9d94e4de8d45da1761017536d
Tarek Khaled <tarekkhaled5112@gmail.com>
2026-09-21 21:54:38 +0300

    Create validator for CreateProductCommand using fluent validation

--- src/Services/Catalog/Catalog.API/GlobalUsing.cs
+++ src/Services/Catalog/Catalog.API/GlobalUsing.cs
@@ -5,6 +5,8 @@ global using BuldingBlocks.CQRS;
 global using Catalog.API.Models;
 global using Marten;
 global using Catalog.API.Exceptions;
+global using FluentValidation;
+
 
 
 
--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -6,8 +6,20 @@
 
     public record CreateProductResult(Guid Id);
 
-    internal class CreateProductHandler(IDocumentSession session) 
-        : ICommandHandler<CreateProductCommand, CreateProductResult>
+    public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
+    {
+        public CreateProductCommandValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required.");
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Description is required.");
+            RuleFor(x => x.ImageUrl).NotEmpty().WithMessage("Image URL is required.");
+            RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be greater than 0.");
+            RuleFor(x => x.Category).NotEmpty().WithMessage("Category is required.");
+        }
+    }
+
+    internal class CreateProductHandler(IDocumentSession session)
+            : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
         {
