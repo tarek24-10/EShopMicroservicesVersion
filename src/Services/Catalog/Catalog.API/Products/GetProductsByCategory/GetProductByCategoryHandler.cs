9a6ef0b3fec7297708f26a5b4de6ef34cef8e950
Tarek Khaled <tarekkhaled5112@gmail.com>
2026-09-21 19:17:45 +0300

    Develop CreateProductByCategory feature

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductsByCategory/GetProductByCategoryHandler.cs
@@ -0,0 +1,20 @@
+﻿namespace Catalog.API.Products.GetProductsByCategory
+{
+    public record GetProductByCategoryQuery(string Category) : IQuery<GetProductByCategoryResult>;
+    public record GetProductByCategoryResult(IEnumerable<Product> Products);
+    public class GetProductByCategoryHandler(IDocumentSession session
+        , ILogger<GetProductByCategoryHandler> logger) : IQueryHandler<GetProductByCategoryQuery, GetProductByCategoryResult>
+    {
+        public async Task<GetProductByCategoryResult> Handle(GetProductByCategoryQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("Handling GetProductByCategoryQuery for category: {Category}"
+                , query.Category);
+
+            var products = await session.Query<Product>()
+                .Where(p => p.Category.Contains(query.Category))
+                .ToListAsync();
+
+            return new GetProductByCategoryResult(products);
+        }
+    }
+}
