a2285b5186ff71e2d1f6b05dccd3f29dee73486c
Tarek Khaled <tarekkhaled5112@gmail.com>
2026-09-21 13:29:23 +0300

    Adding mediator & carter configurations

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -2,7 +2,23 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddMediatR(configuration =>
+configuration.RegisterServicesFromAssembly(typeof(Program).Assembly));
+
+builder.Services.AddCarter(null, configurator =>
+{
+    var moduleTypes = typeof(Program).Assembly
+        .GetTypes()
+        .Where(t => typeof(ICarterModule).IsAssignableFrom(t) && !t.IsAbstract)
+        .ToArray();
+
+    configurator.WithModules(moduleTypes);
+});
+
+
 var app = builder.Build();
+
+app.MapCarter();
 
 // Configure the HTTP request pipeline.
 
