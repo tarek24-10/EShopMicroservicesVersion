b409e335b74df425825ad6ce19ae5f9dfa80b204
Tarek Khaled <tarekkhaled5112@gmail.com>
2026-09-22 17:40:41 +0300

    Register global exception handling

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,3 +1,5 @@
+using BuldingBlocks.Exceptions.Handler;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -19,6 +21,8 @@ builder.Services.AddMediatR(configuration =>
     configuration.AddOpenBehavior(typeof(LoggingBehaviour<,>));
 });
 
+builder.Services.AddValidatorsFromAssembly(currentAssembly);
+
 builder.Services.AddMarten(options =>
 {
     options.Connection(builder.Configuration.GetConnectionString("Database")!);
@@ -27,10 +31,14 @@ builder.Services.AddMarten(options =>
 
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
 
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
 app.MapCarter();
+
+app.UseExceptionHandler(options => { });
 
 app.Run();
