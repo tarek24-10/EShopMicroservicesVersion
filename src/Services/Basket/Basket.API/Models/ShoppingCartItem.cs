d0a94b5301ee3acbf395423c59c3b254895d471b
Tarek Khaled <tarekkhaled5112@gmail.com>
2026-09-22 15:02:28 +0300

    Develop basket microservice domain entities

---
+++ src/Services/Basket/Basket.API/Models/ShoppingCartItem.cs
@@ -0,0 +1,12 @@
+﻿namespace Basket.API.Models
+{
+    public class ShoppingCartItem
+    {
+        public int ProductId { get; set; } = default;
+        public string ProductName { get; set; } = default!;
+        public int Quantity { get; set; } = default;
+        public decimal Price { get; set; } = default;
+        public string Color { get; set; } = default!;
+
+    }
+}
