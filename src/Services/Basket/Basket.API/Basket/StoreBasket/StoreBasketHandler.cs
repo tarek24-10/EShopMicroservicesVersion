3a654063192010cd003052572ce0c78e9de935ad
Tarek Khaled <tarekkhaled5112@gmail.com>
2026-09-23 19:37:36 +0300

    Consuming dicount grpc service from basket microservice during create or update basket

--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -1,4 +1,6 @@
-﻿namespace Basket.API.Basket.StoreBasket
+﻿using Discount.Grpc.Protos;
+
+namespace Basket.API.Basket.StoreBasket
 {
     public record StoreBasketCommand(ShoppingCart ShoppingCart) : ICommand<StoreBasketResult>;
     public record StoreBasketResult(string UserName);
@@ -10,12 +12,25 @@
             RuleFor(x => x.ShoppingCart.UserName).NotEmpty().WithName("User Name is required");
         }
     }
-    internal class StoreBasketCommandHandler(IBasketRepository basketRepository) : ICommandHandler<StoreBasketCommand, StoreBasketResult>
+    internal class StoreBasketCommandHandler(IBasketRepository basketRepository
+        , DiscountProtoService.DiscountProtoServiceClient discountProto) 
+        : ICommandHandler<StoreBasketCommand, StoreBasketResult>
     {
         public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
         {
+            await ApplyDiscount(command.ShoppingCart, cancellationToken);
             await basketRepository.StoreBasket(command.ShoppingCart, cancellationToken);
             return new StoreBasketResult(command.ShoppingCart.UserName);
+        }
+
+        private async Task<ShoppingCart> ApplyDiscount(ShoppingCart shoppingCart, CancellationToken cancellationToken)
+        {
+            foreach (var item in shoppingCart.Items)
+            {
+                var coupon = await discountProto.GetDiscountAsync(new GetDiscountRequest { ProductName = item.ProductName }, cancellationToken: cancellationToken);
+                item.Price -= (Decimal)coupon.Amount * item.Price;
+            }
+            return shoppingCart;
         }
     }
 }
