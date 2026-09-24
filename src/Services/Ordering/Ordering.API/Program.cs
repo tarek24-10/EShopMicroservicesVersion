ae2c1f6d40eeadcb32df637975fc5365e43f390d
Tarek Khaled <tarekkhaled5112@gmail.com>
2026-09-25 02:00:20 +0300

    Create auto migrate ef core entities when the application startup extension method

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,5 +1,6 @@
 using Ordering.Application;
 using Ordering.Infrastructure;
+using Ordering.Infrastructure.Data.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,5 +15,10 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 
 app.UseApiServices();
+
+if (app.Environment.IsDevelopment())
+{
+    await app.InitializeDatabaseAsync();
+}
 
 app.Run();
