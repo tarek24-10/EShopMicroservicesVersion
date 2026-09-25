4fd54effa7c9a52c0fd7ea7d7010077d1ef4ba17
Tarek Khaled <tarekkhaled5112@gmail.com>
2026-09-25 23:34:52 +0300

    Developing DeleteOrder endpoint

--- src/Services/Ordering/Ordering.API/Endpoints/DeleteOrder.cs
+++ src/Services/Ordering/Ordering.API/Endpoints/DeleteOrder.cs
@@ -1,6 +1,25 @@
-﻿namespace Ordering.API.Endpoints
+﻿using Ordering.Application.Orders.Commands.DeleteOrder;
+
+namespace Ordering.API.Endpoints
 {
-    public class DeleteOrder
+    //public record DeleteOrderRequest(int OrderId);
+    public record DeleteOrderResponse(bool IsSuccess);
+    public class DeleteOrder : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapDelete("/orders/{id}", async (Guid id, ISender sender) =>
+            {
+                var command = new DeleteOrderCommand(id);
+                var result = await sender.Send(command);
+                var response = result.Adapt<DeleteOrderResponse>();
+                return Results.Ok(response);
+            })
+            .WithName("DeleteOrder")
+            .Produces<DeleteOrderResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Deletes an existing order")
+            .WithDescription("Deletes an existing order with the provided ID and returns the deletion status.");
+        }
     }
 }
--- src/Services/Ordering/Ordering.Application/Orders/Commands/DeleteOrder/DeleteOrderCommand.cs
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/DeleteOrder/DeleteOrderCommand.cs
@@ -2,7 +2,7 @@
 
 namespace Ordering.Application.Orders.Commands.DeleteOrder
 {
-    public record DeleteorderCommand(Guid OrderId) : ICommand<DeleteOrderResult>;
+    public record DeleteOrderCommand(Guid OrderId) : ICommand<DeleteOrderResult>;
     public record DeleteOrderResult(bool IsSuccess);
     public class DeleteOrderCommandValidator : AbstractValidator<DeleteorderCommand>
     {
