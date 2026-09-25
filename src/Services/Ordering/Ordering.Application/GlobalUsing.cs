09600800d8c42371e549a8e5ee684095af561f44
Tarek Khaled <tarekkhaled5112@gmail.com>
2026-09-25 17:28:49 +0300

    Develop domain events handlers with using mediatr INotificationHandler

--- src/Services/Ordering/Ordering.Application/GlobalUsing.cs
+++ src/Services/Ordering/Ordering.Application/GlobalUsing.cs
@@ -4,5 +4,12 @@ global using Ordering.Application.Dtos;
 global using Ordering.domain.Models;
 global using Ordering.domain.ValueObjects;
 global using Ordering.Application.Exceptions;
+global using MediatR;
+global using Ordering.domain.Events;
+global using Microsoft.Extensions.Logging;
+
+
+
+
 
 
