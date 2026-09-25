2ba5572831f8010e1023bab9e3b4ae1b516b9840
Tarek Khaled <tarekkhaled5112@gmail.com>
2026-09-25 16:05:25 +0300

    Develop IApplicationDbContext for injection dbcontext according to clean architecture

---
+++ src/Services/Ordering/Ordering.Application/Data/IApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.domain.Models;
+
+namespace Ordering.Application.Data
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<Order> Orders { get; }
+        DbSet<OrderItem> OrderItems { get; }
+        DbSet<Customer> Customers { get; }
+        DbSet<Product> Products { get; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
--- src/Services/Ordering/Ordering.Infrastructure/Data/ApplicationDbContext.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
-﻿using Ordering.domain.Models;
+﻿using Ordering.Application.Data;
+using Ordering.domain.Models;
 using System.Reflection;
 
 namespace Ordering.Infrastructure.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : DbContext, IApplicationDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {}
