363b62eb118ac2925949d1ed2a46663fe52614b0
Tarek Khaled <tarekkhaled5112@gmail.com>
2026-09-25 18:39:30 +0300

    Develop pagination classes in shared library

---
+++ src/BuildingBlocks/BuldingBlocks/Pagination/PaginatedResult.cs
@@ -0,0 +1,5 @@
+﻿namespace BuldingBlocks.Pagination
+{
+    public record PaginatedResult<TEntity>(int PageIndex, int PageSize, long Count
+        , IEnumerable<TEntity> Data) where TEntity : class;
+}
---
+++ src/BuildingBlocks/BuldingBlocks/Pagination/PaginationRequest.cs
@@ -0,0 +1,4 @@
+﻿namespace BuldingBlocks.Pagination
+{
+    public record PaginationRequest(int PageIndex = 0, int PageSize = 10);
+}
