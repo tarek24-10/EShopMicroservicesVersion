512fc1d846ee159df1911ae5d4c87ac0c1234400
Tarek Khaled <tarekkhaled5112@gmail.com>
2026-09-25 16:55:59 +0300

    Develop DeleteOrder usecase

---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/DeleteOrder/DeleteOrderCommand.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace Ordering.Application.Orders.Commands.DeleteOrder
+{
+    public record DeleteorderCommand(Guid OrderId) : ICommand<DeleteOrderResult>;
+    public record DeleteOrderResult(bool IsSuccess);
+    public class DeleteOrderCommandValidator : AbstractValidator<DeleteorderCommand>
+    {
+        public DeleteOrderCommandValidator()
+        {
+            RuleFor(x => x.OrderId).NotEmpty().WithMessage("OrderId is required.");
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/DeleteOrder/DeleteOrderHandler.cs
@@ -0,0 +1,21 @@
+﻿namespace Ordering.Application.Orders.Commands.DeleteOrder
+{
+    public class DeleteOrderHandler(IApplicationDbContext dbContext) : ICommandHandler<DeleteorderCommand, DeleteOrderResult>
+    {
+        public async Task<DeleteOrderResult> Handle(DeleteorderCommand request
+            , CancellationToken cancellationToken)
+        {
+            var orderId = OrderId.Of(request.OrderId);
+            var order = await dbContext.Orders.FindAsync([orderId], cancellationToken);
+            if (order is null)
+            {
+                throw new OrderNotFoundException(request.OrderId);
+            }
+
+            dbContext.Orders.Remove(order);
+            await dbContext.SaveChangesAsync(cancellationToken);
+
+            return new DeleteOrderResult(true);
+        }
+    }
+}
