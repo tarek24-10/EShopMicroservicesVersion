10fd9632a75070a8f2522fb66c3422d795814685
Tarek Khaled <tarekkhaled5112@gmail.com>
2026-09-25 23:25:52 +0300

    Developing CreateOrder endpoint

--- src/Services/Ordering/Ordering.API/Endpoints/CreateOrder.cs
+++ src/Services/Ordering/Ordering.API/Endpoints/CreateOrder.cs
@@ -1,6 +1,24 @@
-﻿namespace Ordering.API.Endpoints
+﻿using Ordering.Application.Orders.Commands.CreateOrder;
+namespace Ordering.API.Endpoints
 {
-    public class CreateOrder
+    public record CreateOrderRequest(OrderDto Order);
+    public record CreateOrderResponse(Guid OrderId);
+    public class CreateOrder : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/orders", async (CreateOrderRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<CreateOrderCommand>();
+                var result = await sender.Send(command);
+                var responce = result.Adapt<CreateOrderResponse>();
+                return Results.Created($"/orders/{responce.OrderId}", responce);
+            })
+            .WithName("CreateOrder")
+            .Produces<CreateOrderResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Creates a new order")
+            .WithDescription("Creates a new order with the provided details and returns the created order's ID.");
+        }
     }
 }
