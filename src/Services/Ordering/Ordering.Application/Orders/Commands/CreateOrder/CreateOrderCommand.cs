e04d3697db35ac0ec6b094a2c1c8fb7b5a8383c3
Tarek Khaled <tarekkhaled5112@gmail.com>
2026-09-25 15:52:12 +0300

    Develop CreateOrderCommand

---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderCommand.cs
@@ -0,0 +1,18 @@
+﻿using BuldingBlocks.CQRS;
+using FluentValidation;
+using Ordering.Application.Dtos;
+
+namespace Ordering.Application.Orders.Commands.CreateOrder
+{
+    public record CreateOrderCommand(OrderDto Order) : ICommand<CreateOrderResult>;
+    public record CreateOrderResult(Guid OrderId);
+    public class CreateOrderCommandValidator : AbstractValidator<CreateOrderCommand>
+    {
+        public CreateOrderCommandValidator()
+        {
+            RuleFor(x => x.Order.OrderName).NotEmpty().WithMessage("Order name is required");
+            RuleFor(x => x.Order.CustomerId).NotNull().WithMessage("Customer ID is required");
+            RuleFor(x => x.Order.Items).NotEmpty().WithMessage("Order items are required");
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderHandler.cs
@@ -0,0 +1,14 @@
+﻿using BuldingBlocks.CQRS;
+namespace Ordering.Application.Orders.Commands.CreateOrder
+{
+    public class CreateOrderCommandHandler : ICommandHandler<CreateOrderCommand, CreateOrderResult>
+    {
+        public async Task<CreateOrderResult> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
+        {
+            return await Task.FromResult(new CreateOrderResult(request.Order.Id)
+            {
+                OrderId = Guid.NewGuid()
+            });
+        }
+    }
+}
