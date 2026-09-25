a20dcc3e5497f7ab414ec7efd824e623a4991c78
Tarek Khaled <tarekkhaled5112@gmail.com>
2026-09-25 23:43:41 +0300

    Developing GetOrdersByCustomer endpoint

--- src/Services/Ordering/Ordering.API/Endpoints/GetOrdersByCustomer.cs
+++ src/Services/Ordering/Ordering.API/Endpoints/GetOrdersByCustomer.cs
@@ -1,6 +1,26 @@
-﻿namespace Ordering.API.Endpoints
+﻿using Ordering.Application.Orders.Queries.GetOrdersbyCustomer;
+
+namespace Ordering.API.Endpoints
 {
-    public class GetOrdersByCustomer
+    //public record GetOrdersByCustomerRequest(Guid CustomerId);
+    public record GetOrdersByCustomerResponse(IEnumerable<OrderDto> Orders);
+    public class GetOrdersByCustomer : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/orders/{customerId:guid}", async (Guid customerId, ISender sender) =>
+            {
+                var query = new GetOrdersByCustomerQuery(customerId);
+                var result = await sender.Send(query);
+                var response = result.Adapt<GetOrdersByCustomerResponse>();
+                return Results.Ok(response);
+            })
+            .WithName("GetOrdersByCustomer")
+            .Produces<GetOrdersByCustomerResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .WithSummary("Get orders by customer")
+            .WithDescription("Get orders by customer");
+        }
     }
 }
