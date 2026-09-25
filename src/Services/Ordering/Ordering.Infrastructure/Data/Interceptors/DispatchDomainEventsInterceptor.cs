25df05a643b2a7e682dc76159b9bbe53dc7eec71
Tarek Khaled <tarekkhaled5112@gmail.com>
2026-09-25 04:31:21 +0300

    Deveolp DispatchDomainEventsInterceptor to trigger domain events handlers

---
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Interceptors/DispatchDomainEventsInterceptor.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace Ordering.Infrastructure.Data.Interceptors
+{
+    public class DispatchDomainEventsInterceptor(IMediator mediator) : SaveChangesInterceptor
+    {
+        public override InterceptionResult<int> SavingChanges(DbContextEventData eventData, InterceptionResult<int> result)
+        {
+            DispatchDomainEvents(eventData.Context).GetAwaiter().GetResult();
+            return base.SavingChanges(eventData, result);
+        }
+
+        public override async ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData, InterceptionResult<int> result, CancellationToken cancellationToken = default)
+        {
+            await DispatchDomainEvents(eventData.Context);
+            return await base.SavingChangesAsync(eventData, result, cancellationToken);
+        }
+
+        private async Task DispatchDomainEvents(DbContext? context)
+        {
+            if (context is null) return;
+
+            var aggregates = context?.ChangeTracker.Entries<IAggregate>()
+                .Where(e => e.Entity.DomainEvents.Any()).Select(e => e.Entity).ToList();
+
+            var domainEvents = aggregates?.SelectMany(a => a.DomainEvents).ToList();
+
+            aggregates?.ForEach(a => a.ClearDomainEvents());
+
+            domainEvents?.ForEach(de => mediator.Publish(de));
+        }
+    }
+}
